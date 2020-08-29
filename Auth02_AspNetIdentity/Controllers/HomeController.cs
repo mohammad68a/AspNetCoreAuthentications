1ad9d44c5f6699d64c349635fa62411eb663ee55
Mohammad Ahmadi <webdev.ahmadi@gmail.com>
2020-08-29 12:08:44 +0430

    Add email verification process

--- Auth02_AspNetIdentity/Controllers/HomeController.cs
+++ Auth02_AspNetIdentity/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NETCore.MailKit.Core;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Auth02_AspNetIdentity.Controllers
@@ -10,11 +12,16 @@ namespace Auth02_AspNetIdentity.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IEmailService _emailService;
 
-        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public HomeController(
+            UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager,
+            IEmailService emailService)
         {
             _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
             _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+            _emailService = emailService ?? throw new ArgumentNullException(nameof(emailService));
         }
 
         [HttpGet]
@@ -74,6 +81,9 @@ namespace Auth02_AspNetIdentity.Controllers
 
             if (result.Succeeded)
             {
+                // Generation of the email token
+                await SendVerificationEmailAsync(user);
+
                 // Sign in user here.
                 var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
 
@@ -88,11 +98,38 @@ namespace Auth02_AspNetIdentity.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        [NonAction]
+        private async Task SendVerificationEmailAsync(IdentityUser user)
+        {
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            var link = Url.Action(nameof(VerifyEmail), "Home", new { userId = user.Id, code }, Request.Scheme, Request.Host.ToString());
+
+            await _emailService.SendAsync(user.Email, "Email Verification", $"<a href=\"{link}\">Verify email</a>", true);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> VerifyEmail(string userId, string code)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null) return BadRequest("OOPS, Somethings went wrong!");
+
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Login));
+            }
+
+            return View();
+        }
+
         [HttpGet, HttpPost]
         public async Task<IActionResult> LogOut()
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction(nameof(Index));
         }
-    } 
+    }
 }
--- Auth02_AspNetIdentity/Startup.cs
+++ Auth02_AspNetIdentity/Startup.cs
@@ -3,13 +3,22 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NETCore.MailKit.Extensions;
+using NETCore.MailKit.Infrastructure.Internal;
 
 namespace Auth02_AspNetIdentity
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -29,7 +38,7 @@ namespace Auth02_AspNetIdentity
                     config.Password.RequireUppercase = false;
                     config.Password.RequireNonAlphanumeric = false;
 
-                    config.SignIn.RequireConfirmedEmail = false;
+                    config.SignIn.RequireConfirmedEmail = true;
                     config.SignIn.RequireConfirmedAccount = false;
                     config.SignIn.RequireConfirmedPhoneNumber = false;
                 })
@@ -42,6 +51,11 @@ namespace Auth02_AspNetIdentity
                     config.LoginPath = "/home/login";
                     config.Cookie.Name = "Identity.Cookie";
                 });
+
+            services.AddMailKit(config =>
+            {
+                config.UseMailKit(_configuration.GetSection(nameof(MailKitOptions)).Get<MailKitOptions>());
+            });
 
             services.AddControllersWithViews();
         }
