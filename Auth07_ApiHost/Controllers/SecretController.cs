e7b50242fbfb4af1fc424c31e88afcc9e9311fc6
Mohammad Ahmadi <webdev.ahmadi@gmail.com>
2020-09-15 01:24:12 +0430

    Add project [Auth07_ApiHost] project

---
+++ Auth07_ApiHost/Controllers/SecretController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth07_ApiHost.Controllers
+{
+    public class SecretController : Controller
+    {
+        [Authorize]
+        public string Index()
+        {
+            return "Secret message";
+        }
+    }
+}
---
+++ Auth07_ApiHost/Startup.cs
@@ -0,0 +1,54 @@
+using Auth07_ApiHost.Requirements;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Auth07_ApiHost
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication();
+
+            services.AddAuthorization(config =>
+            {
+                var defaultAuthBuilder = new AuthorizationPolicyBuilder();
+                var defaultAuthPolicy = defaultAuthBuilder
+                    .AddRequirements(new JwtRequirement())
+                    .Build();
+                config.DefaultPolicy = defaultAuthPolicy;
+            });
+
+            services.AddScoped<IAuthorizationHandler, JwtRequirementHandler>();
+
+            services.AddHttpClient().AddHttpContextAccessor();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
